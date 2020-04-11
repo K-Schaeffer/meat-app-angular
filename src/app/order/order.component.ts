61da0ba06208e037d0f917ddd9a5158dcb1b23d0
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-04-11 16:53:50 -0300

    Adding radio component - basic payment radios

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions: RadioOption[] = [
+    { label: 'Cash', value: 'MON' },
+    { label: 'Debit', value: 'DEB' },
+    { label: 'Credit', value: 'CRED' }
+  ]
 
   constructor() { }
 
